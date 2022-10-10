8808f53857262d576f2d50a680f93aca509c550b
santi <santimartinezalbert02@gmail.com>
2022-10-10 11:55:47 +0200

    dummies

--- backend/app/routes/api/dummy.js
+++ backend/app/routes/api/dummy.js
@@ -3,13 +3,17 @@ var mongoose = require('mongoose');
 var Product = mongoose.model('product');
 var Category = mongoose.model('category');
 const category = require("../../controllers/category.controller.js");
+const { product } = require("../../models/index.js");
 var dummies = require('../../utils/dummy.json');
 
 router.get("/", (req, res) => {
     try {
         Category.collection.drop();
+        Product.collection.drop();
+        let product = [];
         let category = [];
-        dummies.map(dummy => {
+
+        dummies[0].map(dummy => {
             category.push(new Category({
                 slug: dummy.slug,
                 id_cat: dummy.id_cat || null,
@@ -18,6 +22,19 @@ router.get("/", (req, res) => {
                 img_cat2: dummy.img_cat2 || null,
             }))
         })
+        dummies[1].map(dummy => {
+            product.push(new Product({
+                prod_nom: dummy.prod_nom || null,
+                slug: dummy.slug || null,
+                id_prod_typ: dummy.id_prod_typ || null,
+                prod_desc: dummy.prod_desc || null,
+                price: dummy.price || null,
+                img_prod: dummy.img_prod || null,
+                id_prod_cat: dummy.id_prod_cat || null
+            }))
+        })
+
+        Product.insertMany(product);
         Category.insertMany(category);
         res.send("All good");
     } catch (err) {
