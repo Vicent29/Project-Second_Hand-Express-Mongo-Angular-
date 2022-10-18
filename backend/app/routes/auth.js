023a3cc7cc37e4ab2d6ab15a820bf764cdc9b23b
santi <santimartinezalbert02@gmail.com>
2022-10-18 16:04:52 +0200

    login/register/profile backend

--- backend/app/routes/auth.js
+++ backend/app/routes/auth.js
@@ -2,10 +2,8 @@ var { expressjwt: jwt } = require('express-jwt');
 var secret = require('../config').secret;
 
 function getTokenFromHeader(req) {
-    console.log("hola");
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
         req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            console.log(req.headers.authorization.split(' ')[1]);
         return req.headers.authorization.split(' ')[1];
     }
 
@@ -16,16 +14,16 @@ var auth = {
     required: jwt({
         secret: secret,
         algorithms: ["HS256"],
-        userProperty: 'payload',
+        userProperty: "payload",
         getToken: getTokenFromHeader
     }),
-    // optional: jwt({
-    //     secret: secret,
-    //     algorithms: ["HS256"],
-    //     userProperty: 'payload',
-    //     credentialsRequired: false,
-    //     getToken: getTokenFromHeader
-    // })
+    optional: jwt({
+        secret: secret,
+        algorithms: ["HS256"],
+        userProperty: 'payload',
+        credentialsRequired: false,
+        getToken: getTokenFromHeader
+    })
 };
 
 module.exports = auth;
